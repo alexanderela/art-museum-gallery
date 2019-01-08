c14f19b5633b876f66f052c934ab7ae2cdaac1fa
Alexander Ela <ela.alexander@gmail.com>
2019-01-07 21:58:37 -0700

    Add basic HTML and CSS

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+Vue.component('todo-item', {
+	props: ['todo'],
+	template: '<li>{{ todo.text }}</li>'
+})
+
 var app = new Vue({
 	el: '#app',
 	data: {
-		message: 'Well, hello there'
-	},
-	methods: {
-		reverseMessage: function() {
-			this.message = this.message.split('').reverse().join('')
-		}
+		groceryList: [
+			{ id: 0, text: 'Vegetables' },
+			{ id: 1, text: 'Cheese' },
+			{ id: 2, text: 'Whatever else humans eat' },
+		]
 	}
 });
 
