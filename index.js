3af5ebd8a342cc74ed141ec601b66c3c6933b28a
Alexander Ela <ela.alexander@gmail.com>
2019-01-08 20:22:14 -0700

    Create cleanArtworkData function to format data

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ Vue.component('artwork-item', {
 	template: `
 	<div>
 		<img
-			v-if='artwork.primaryimageurl !== null' 
+			v-if='artwork.image !== null' 
 			class='artwork-image' 
-			v-bind:src='artwork.primaryimageurl'/>
+			v-bind:src='artwork.image'/>
 		<div v-else
 			class='no-image-container'>
 			<p class='no-image'>No image available</p>
@@ -27,9 +27,29 @@ var app = new Vue({
 	data: {
 		artworksList: []
 	},
+	methods: {
+		cleanArtworkData(fetchedArtworkData) {
+			const cleanedArtworkData = fetchedArtworkData.map(artwork => {
+
+			const { primaryimageurl, title, people, technique, medium, dated } = artwork
+
+				return {
+					image: primaryimageurl,
+					title: title,
+					artist: 'Artist Unknown',
+					medium: technique || medium,
+					date: dated
+				}
+				
+			})
+
+			console.log(cleanedArtworkData)
+			return cleanedArtworkData
+		}
+	},
 	mounted() {
 		axios.get(`https://api.harvardartmuseums.org/object?size=100&height=150&width=150&apikey=${APIKey}`)
-			.then(response => { this.artworksList = response.data.records })
+			.then(response => { this.artworksList = this.cleanArtworkData(response.data.records) })
 			.catch(error => console.log(error))
 	}
 });
